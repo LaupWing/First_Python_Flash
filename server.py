75c78c0f0bff37dbd74ffc937e1a25e157c9506b
LaupWing <loc_strict@hotmail.com>
2020-04-02 00:08:58 +0200

    added database

--- server.py
+++ server.py
@@ -12,10 +12,18 @@ def load_html(page_name):
     return render_template(page_name)
 
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        database.write(f'\n{email}, {subject}, {message}')
+
+
 @app.route('/form_submit', methods=['POST', 'GET'])
 def form_submit():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect('/thankyou.html')
     return 'Something wen wrong'
