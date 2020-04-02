dc2a3460c50b89e807d681283b66b370a4b35791
LaupWing <loc_strict@hotmail.com>
2020-04-02 18:00:29 +0200

    trying ot add databse

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 app = Flask(__name__)
 
 
@@ -20,10 +21,23 @@ def write_to_file(data):
         database.write(f'\n{email}, {subject}, {message}')
 
 
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route('/form_submit', methods=['POST', 'GET'])
 def form_submit():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Couldn save it to the database'
     return 'Something wen wrong'
